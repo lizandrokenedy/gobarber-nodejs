cec7cbeadaebd667ec0b2cf72e4163f8d06f927e
Lizandro Kenedy <lizandrosantos@live.com>
2021-02-27 12:17:01 -0400

    Tratativa de erros de forma global

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -16,7 +16,6 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
     const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
@@ -29,9 +28,6 @@ appointmentsRouter.post('/', async (request, response) => {
     });
 
     return response.json(appointment);
-  } catch (error) {
-    return response.status(400).json({ error: error.message });
-  }
 });
 
 export default appointmentsRouter;
--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -4,7 +4,7 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
+
     const { email, password } = request.body;
 
     const authenticateUser = new AuthenticateUserService();
@@ -12,9 +12,7 @@ sessionsRouter.post('/', async (request, response) => {
     const { user, token } = await authenticateUser.execute({email, password});
 
     return response.json({ user, token });
-  } catch (error) {
-    return response.status(error.statusCode).json({ error: error.message });
-  }
+
 });
 
 export default sessionsRouter;
